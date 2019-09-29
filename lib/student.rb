0e0be31619a0414730668231c5d4080a2d8194c5
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-29 23:27:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,10 +37,10 @@ class Student < InteractiveRecord
   #   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   # end
   #
-  # def table_name_for_insert
-  #   self.class.table_name
-  # end
-  #
+  def table_name_for_insert
+    self.class.table_name
+  end
+
   # def values_for_insert
   #   values = []
   #   self.class.column_names.each do |col_name|
